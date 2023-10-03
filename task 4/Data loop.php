9385f2972fd10c06b813cb95c4a0bc19e388e76d
kareemshellbaya <kareemshellbaya@gmail.com>
2023-10-04 02:59:54 +0300

    Task 4 Finshed

--- task 4/Data loop.php
+++ task 4/Data loop.php
@@ -2,4 +2,28 @@
 
     require_once("Data.php");
 
+
+    
+    foreach($countryList as $key => $state){
+
+        echo"$key => $state <br>";
+
+    }
+
+    echo"<hr>";
+    
+    foreach($stateList['CA'] as $key => $state){
+        
+        echo"$key => $state <br>";
+        
+    }
+
+    echo"<hr>";
+    
+    foreach($stateList['US'] as $key => $state){
+        
+        echo"$key => $state <br>";
+        
+    }
+    echo"<hr>";
 ?>
--- task 4/Data.php
+++ task 4/Data.php
@@ -75,26 +75,4 @@ $stateList['US'] = [
     'WY'        => 'Wyoming'
     ];
 
-
-    foreach($countryList as $key => $state){
-
-        echo"$key => $state <br>";
-
-    }
-
-    echo"<hr>";
-    
-    foreach($stateList['CA'] as $key => $state){
-        
-        echo"$key => $state <br>";
-        
-    }
-
-    echo"<hr>";
-    
-    foreach($stateList['US'] as $key => $state){
-        
-        echo"$key => $state <br>";
-        
-    }
-    echo"<hr>";
+?>
