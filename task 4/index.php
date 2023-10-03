0477b7b564b15b6daa5dbfc7112c2c67abbc5207
kareemshellbaya <kareemshellbaya@gmail.com>
2023-10-03 11:46:37 +0300

    Task 4 Is Finshed

---
+++ task 4/index.php
@@ -0,0 +1,21 @@
+
+
+
+<?php 
+# Including Header From layout Folder
+  include_once"layout/header.php";
+?>
+
+
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam mollitia natus inventore? Natus voluptatibus quidem hic libero cumque? Doloremque eaque expedita maiores voluptate odit cum inventore provident blanditiis pariatur dolorum.
+    Impedit deleniti repellendus sit iste mollitia illo aut facilis eum labore, animi dolorem qui odio dicta voluptatibus rerum molestias quibusdam. Dignissimos temporibus praesentium voluptate explicabo assumenda aperiam tempore neque exercitationem.
+    Vero iste totam maxime quos asperiores consequatur accusamus, quis incidunt sint blanditiis aut exercitationem modi amet eum. Natus voluptas optio officia commodi amet, ut maxime eaque velit, tempore quos modi.
+  </p>
+        
+      
+
+<?php 
+# Including Footer From layout Folder
+  include_once"layout/footer.php";
+?>
